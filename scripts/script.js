6720d646593048f7076c132851a37db5a1b1d5ab
Norvin <chnorvin@gmail.com>
2022-11-08 15:15:26 -0800

    Final Script Actvity

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,19 @@
 // for (let i = 1;  i <= 10; i++ ){
 //     console.log(2 ** i);
 // }
-let n = 1
-while (n <=10){
-    console.log(2 ** n)
-    n = n + 1
-}
+// let n = 1
+// while (n <=10){
+//     console.log(2 ** n)
+//     n = n + 1
+// }
+let input;
+do{
+input = prompt ("what is your name?");
+//prompt always returns string
+//convert input to number
+console.log(Number(input));
+} while (!isNaN(Number(input)));
+
+// alert("Hello" + input);
+alert(`Hello ${input}`);
+
