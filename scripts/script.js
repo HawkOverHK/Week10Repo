e973b8f98a93773b8a311983d044375848da6e72
Norvin <chnorvin@gmail.com>
2022-11-08 14:48:24 -0800

    New Script Activities done

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// for (let i = 0; i < 1000000; i++) {console.log(i);}
+// for (let i = 1;  i <= 10; i++ ){
+//     console.log(2 ** i);
+// }
+let n = 1
+while (n <=10){
+    console.log(2 ** n)
+    n = n + 1
+}
